6fe769c57e15ea4c01f617e62503bed638c0543f
Evgenia Neverovskaya <evgenianeverovskaya@gmail.com>
2024-04-12 11:44:44 +0300

    Add a section describing the platform

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Header from './components/header';
+import Main from './pages/main';
 
 function App() {
   return (
     <ChakraProvider>
       <Header/>
+      <Main/>
   </ChakraProvider>
   );
 }
---
+++ src/pages/main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Container, Text, Grid, GridItem, Heading} from "@chakra-ui/react";
+
+const Main = () => {
+  return (
+    <Box as='section' bg='#B23386'>
+      <Container maxW='8xl'>
+        <Box color='white' minH="730px">
+          <Grid
+            templateColumns='repeat(7, 1fr)'
+            templateRows='repeat(3, 1fr)'
+            gap={0}>
+            <GridItem gridColumn='5 / span 3' gridRow='2 / 3'>
+              <Box textTransform='uppercase' display='flex' flexDirection='column' gap='20px'>
+                <Text>digital platform</Text>
+                <Text>
+                  This is a space for interaction between creators and users,
+                  promotes the distribution of digital content and provides
+                  opportunities for monetization and development in the digital
+                  environment.
+                </Text>
+              </Box>{" "}
+            </GridItem>
+            <GridItem gridColumn='1 / span 3' gridRow='3 / 4'>
+              <Heading fontSize="180px" lineHeight="170px">Digital</Heading>
+            </GridItem>
+            <GridItem gridColumn='4 / span 3' gridRow='4 / 5' position="relative"> 
+              <Heading fontSize="180px" lineHeight="140px" position="absolute" left="3vw"> 
+                Products
+              </Heading>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Main;
