8be5e1a4a186877909f4caee7c6a75f605967f9a
Evgenia Neverovskaya <evgenianeverovskaya@gmail.com>
2024-04-16 20:30:14 +0300

    Pin a header when scrolling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import  {Link, Flex, Container, Box, Text, List, ListItem} from '@chakra-ui/reac
 
 const Header = () => {
     return (
-        <Box as="header" padding="8px">
+        <Box as="header" padding="8px" position="fixed" top={0} w="100%" zIndex="10" bg="white">
             <Container maxW="8xl" textTransform="uppercase">
                 <Flex justifyContent="space-between" alignItems="center">
                     <Text fontWeight="500" fontSize="30px" lineHeight="35.8px">NEOBAZAAR</Text>
