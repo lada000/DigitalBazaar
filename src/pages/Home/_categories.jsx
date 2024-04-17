99205b079af52d813187a89acc90c7bc5f9b0575
Evgenia Neverovskaya <evgenianeverovskaya@gmail.com>
2024-04-17 21:17:41 +0300

    Fix size in categories

--- src/pages/Home/_categories.jsx
+++ src/pages/Home/_categories.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import {
-  Box,
-  Container,
+import { Container,
   Link ,
   Grid,
-  Image,
-  Text,
-  Flex,
-  Button,
   GridItem,
-  Heading,
+  Text,
   Center,
   Divider,
+  Image,
 } from "@chakra-ui/react";
 import Gif3D from "../../image/Categories/Gif3D.gif";
 import Education from "../../image/Categories/education.gif";
@@ -23,6 +18,7 @@ import Photo from "../../image/Categories/photo.gif";
 import Sport from "../../image/Categories/sport.gif";
 import Design from "../../image/Categories/Design.gif";
 import ButtonStartSelling from "../../components/buttonStartSelling";
+import { Box } from "@chakra-ui/react";
 
 const linkCategories = {
   color: "#867777",
@@ -32,7 +28,7 @@ const linkCategories = {
   fontWeight: "400",
 }
 
-const backgroundImageStyle = {
+const backgroundImageStyle  = {
   bgPosition: "center",
   bgRepeat: "no-repeat",
   bgSize: "cover",
@@ -42,7 +38,7 @@ const backgroundImageStyle = {
 
 const Categories = () => {
   return (
-    <Box as='section'>
+    <Box as='section' >
       <Container maxW='8xl'>
         <ButtonStartSelling />
         <Center>
@@ -52,68 +48,70 @@ const Categories = () => {
             Discover the best-selling products and creators
           </Text>
         </Center>
-        
+     <Center>   
     <Grid
       templateColumns="repeat(3, 1fr)"
       templateRows="1fr 0.1fr 1fr 0.1fr 1fr 0.1fr"
-      gap={5}
+      width="80%"
+      columnGap={14}
+      rowGap={1}
     >
-      <Box gridArea="1 / 1 / 2 / 2" bgImage={Gif3D} {...backgroundImageStyle}/>
-      <Box gridArea="2 / 1 / 3 / 2" p="10px 20px">
+      <GridItem gridArea="1 / 1 / 2 / 2" bgImage={Gif3D} {...backgroundImageStyle} />
+      <GridItem gridArea="2 / 1 / 3 / 2" p="10px 20px" gap={0}>
       <Link style={linkCategories}>3D →</Link >
-      </Box>
+      </GridItem>
 
-      <Box gridArea="1 / 2 / 2 / 3" bgImage={Education} {...backgroundImageStyle} > 
-      </Box>
-      <Box gridArea="2 / 2 / 3 / 3" p="10px 20px">
+      <GridItem gridArea="1 / 2 / 2 / 3" bgImage={Education} {...backgroundImageStyle}  > 
+      </GridItem>
+      <GridItem gridArea="2 / 2 / 3 / 3" p="10px 20px">
       <Link style={linkCategories}>EDUCATION →</Link >
-      </Box>
+      </GridItem>
 
 
-      <Box gridArea="1 / 3 / 2 / 4" 
-       bgImage={Audio} {...backgroundImageStyle}>        
-      </Box>
-      <Box gridArea="2 / 3 / 3 / 4" p="10px 20px">      
+      <GridItem gridArea="1 / 3 / 2 / 4" 
+       bgImage={Audio} {...backgroundImageStyle}  />
+      <GridItem gridArea="2 / 3 / 3 / 4" p="10px 20px">      
       <Link style={linkCategories}>AUDIO →</Link >
-      </Box>
+      </GridItem>
 
 
-      <Box gridArea="3 / 1 / 4 / 2"  bgImage={Design} {...backgroundImageStyle}/>
-      <Box gridArea="4 / 1 / 5 / 2" p="10px 20px">
+      <GridItem gridArea="3 / 1 / 4 / 2"  bgImage={Design} {...backgroundImageStyle} />
+      <GridItem gridArea="4 / 1 / 5 / 2" p="10px 20px">
       <Link style={linkCategories}>DESIGN →</Link >
-      </Box>
+      </GridItem>
 
 
-      <Box gridArea="3 / 2 / 4 / 3"  bgImage={Paint} {...backgroundImageStyle} />
-      <Box gridArea="4 / 2 / 5 / 3" p="10px 20px">
+      <GridItem gridArea="3 / 2 / 4 / 3"  bgImage={Paint} {...backgroundImageStyle}  />
+      <GridItem gridArea="4 / 2 / 5 / 3" p="10px 20px">
       <Link style={linkCategories}>DRAWING & PAINTING →</Link >
-      </Box>
+      </GridItem>
 
 
-      <Box gridArea="3 / 3 / 4 / 4" >
-      <Image src={Photo} objectFit="cover" alt='Photo' /> </Box>
-      <Box gridArea="4 / 3 / 5 / 4" p="10px 20px">
+      <GridItem gridArea="3 / 3 / 4 / 4"> 
+      <Image src={Photo} objectFit="cover" alt='Photo' /></GridItem>
+      <GridItem gridArea="4 / 3 / 5 / 4" p="10px 20px">
       <Link style={linkCategories}>PHOTOGRAPHY →</Link >    
-      </Box>
+      </GridItem>
 
 
-      <Box gridArea="5 / 1 / 6 / 2"  bgImage={Game} {...backgroundImageStyle} />
-      <Box gridArea="6 / 1 / 7 / 2" p="10px 20px">
+      <GridItem gridArea="5 / 1 / 6 / 2"  bgImage={Game} {...backgroundImageStyle}  />
+      <GridItem gridArea="6 / 1 / 7 / 2" p="10px 20px">
       <Link style={linkCategories}>GAMING →</Link >   
-         </Box>
+         </GridItem>
 
 
-      <Box gridArea="5 / 2 / 6 / 3" bgImage={Sport} {...backgroundImageStyle} />
-      <Box gridArea="6 / 2 / 7 / 3" p="10px 20px">
+      <GridItem gridArea="5 / 2 / 6 / 3" bgImage={Sport} {...backgroundImageStyle}  />
+      <GridItem gridArea="6 / 2 / 7 / 3" p="10px 20px">
       <Link style={linkCategories}>FITNESS & HEALTH →</Link >     
-      </Box>
+      </GridItem>
 
 
-      <Box gridArea="5 / 3 / 6 / 4"  bgImage={Code} {...backgroundImageStyle} />
-      <Box gridArea="6 / 3 / 7 / 4" p="10px 20px">
+      <GridItem gridArea="5 / 3 / 6 / 4"  bgImage={Code} {...backgroundImageStyle}  />
+      <GridItem gridArea="6 / 3 / 7 / 4" p="10px 20px">
       <Link style={linkCategories}>SOFTWARE DEVELOPMENT →</Link >
-      </Box>
+      </GridItem>
     </Grid>
+    </Center>
       </Container>
     </Box>
   );
