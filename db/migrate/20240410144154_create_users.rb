1ae0b006820d1283143f2f66d5f0cd10badb42bf
lada000 <lavacashy.sinkevich@gmail.com>
2024-04-18 11:26:48 +0200

    add devise

--- db/migrate/20240410144154_create_users.rb
+++ db/migrate/20240410144154_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      # t.string :password
       t.integer :role
 
       t.timestamps
